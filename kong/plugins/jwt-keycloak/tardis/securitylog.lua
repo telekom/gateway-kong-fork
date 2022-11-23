6e993fcc56437c3047fbecd9c7b37adda39b753e
georg.teichert-matuschek@external.telekom.de <georg.teichert-matuschek@external.telekom.de>
2022-11-23 10:30:11 +0000

    Merge branch 'eni-DHEI-9924-on-kong' into 'eni-2.8.3'
    Eni dhei 9924 on kong
    
    See merge request dhei/teams/io/products/stargate/kong!9
    
    (cherry picked from commit 3617d9861ad391bd432186b1ea5232f6c86ac6e4)
    
    dbf673f0 variables declared local as suggested by lua-warnings
    0c2ae92b sec codes updated
    03845e7a store tardis-consumer
    d9fabd4f log security event in acl plugin

--- kong/plugins/jwt-keycloak/tardis/securitylog.lua
+++ kong/plugins/jwt-keycloak/tardis/securitylog.lua
@@ -6,6 +6,17 @@ local function security_event(event_code, event_details)
     ngx.var.sec_event_details=event_details
 end
 
+local function collect_tardis_data(jwt)
+    ngx.var.tardis_consumer="anonymous"
+    if jwt and jwt.claims then
+      local clientId = jwt.claims["clientId"]
+      if type(clientId) == "string" then
+        ngx.var.tardis_consumer=clientId
+      end
+    end
+end
+
 return {
-    security_event = security_event
+    security_event = security_event,
+    collect_tardis_data = collect_tardis_data
 }
