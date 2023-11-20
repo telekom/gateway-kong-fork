f4009b25b1a03474cd0e3f21dca79eb2c5832767
chronolaw <chrono_cpp@me.com>
2023-11-20 16:43:14 +0800

    finally restore spy.on

--- spec/01-unit/01-db/01-schema/06-routes_spec.lua
+++ spec/01-unit/01-db/01-schema/06-routes_spec.lua
@@ -1512,7 +1512,13 @@ describe("routes schema (flavor = expressions)", function()
   end)
 
   it("http route still supports net.port but with warning", function()
+    local ngx_log = ngx.log
     local log = spy.on(ngx, "log")
+
+    finally(function()
+      ngx.log = ngx_log
+    end)
+
     local route = {
       id             = a_valid_uuid,
       name           = "my_route",
